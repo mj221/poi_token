2b5b116a3ab5e4c996d25cd42cfb6a24e0db7d02
MJ LEE <mj1201276@yahoo.com.au>
2021-10-30 21:39:59 +1000

    Delegate transfer

--- contracts/PoiToken.sol
+++ contracts/PoiToken.sol
@@ -13,7 +13,13 @@ contract PoiToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
     
     constructor (uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -27,6 +33,25 @@ contract PoiToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[_from] >= _value);
+        require (_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] = _value;
+
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/PoiToken.js
+++ test/PoiToken.js
@@ -37,4 +37,51 @@ contract('PoiToken', function(accounts){
     //     })
     // })
     
+    it('approves token for delegated transfer', function(){
+        return PoiToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.approve(accounts[1], 100, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'Should be the approval event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorised by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorised to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+        });
+    });
+
+    it('handles delegated token transfers', function(){
+        return PoiToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+        }).then(function(receipt){
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(function(receipt){
+            return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function(success){
+            assert.equal(success, true);
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the approval event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are authorised by');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are authorised to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+        })
+    })
 });
